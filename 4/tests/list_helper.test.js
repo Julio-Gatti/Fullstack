1717a19db0c57a067ad5ee3da6002908d90812af
Julius Sakki <j.sakki@icloud.com>
2022-10-16 22:57:07 +0300

    4.7

--- 4/tests/list_helper.test.js
+++ 4/tests/list_helper.test.js
@@ -74,18 +74,220 @@ describe('Total likes', () => {
     }
   ]
 
-  test('Empty list', () => {
+  test('Empty', () => {
     const result = listHelper.totalLikes(emptyBlogs)
     expect(result).toBe(0)
   })
 
-  test('List of one', () => {
+  test('One', () => {
     const result = listHelper.totalLikes(oneBlogs)
     expect(result).toBe(2)
   })
 
-  test('List of all', () => {
+  test('All', () => {
     const result = listHelper.totalLikes(blogs)
     expect(result).toBe(36)
+  })
+})
+
+describe('Favorite blog', () => {
+  const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    }
+  ]
+
+  const blogWithLikes = {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  }
+
+  const oneBlogs = [
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    }
+  ]
+
+  const emptyBlogs = [
+
+  ]
+
+  test('Empty', () => {
+    const result = listHelper.favoriteBlog(emptyBlogs)
+    expect(result).toEqual(undefined)
+  })
+
+  test('One', () => {
+    const result = listHelper.favoriteBlog(oneBlogs)
+    expect(result).toEqual(blogWithLikes)
+  })
+
+  test('All', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual(blogWithLikes)
+  })
+})
+
+describe('Most blogs', () => {
+  const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    }
+  ]
+
+  const blogWithLikes = {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  }
+
+  const oneBlogs = [
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    }
+  ]
+
+  const emptyBlogs = [
+
+  ]
+
+  test('Empty', () => {
+    const result = listHelper.mostBlogs(emptyBlogs)
+    expect(result).toEqual(undefined)
+  })
+
+  test('All', () => {
+    const result = listHelper.mostBlogs(blogs)
+    const expected = {
+      author: "Edsger W. Dijkstra",
+      blogs: 2
+    }
+    expect(result).toEqual(expected)
+  })
+})
+
+describe('Most likes', () => {
+  const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    }
+  ]
+
+  const blogWithLikes = {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  }
+
+  const oneBlogs = [
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    }
+  ]
+
+  const emptyBlogs = [
+
+  ]
+
+  test('Empty', () => {
+    const result = listHelper.mostLikes(emptyBlogs)
+    expect(result).toEqual(undefined)
+  })
+
+  test('All', () => {
+    const result = listHelper.mostLikes(blogs)
+    const expected = {
+      author: "Edsger W. Dijkstra",
+      likes: 17
+    }
+    expect(result).toEqual(expected)
   })
 })
--- 4/utils/list_helper.js
+++ 4/utils/list_helper.js
@@ -6,7 +6,77 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((max, blog) => {
+    if (blog.likes > (max ? max.likes : 0)) {
+      return blog
+    }
+    return max
+  }, undefined)
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length == 0) {
+    return undefined
+  }
+
+  const amounts = new Map()
+
+  for (let i = 0; i < blogs.length; i++) {
+    const blog = blogs[i]
+
+    if (amounts.has(blog.author)) {
+      amounts.set(blog.author, amounts.get(blog.author) + 1)
+    } else {
+      amounts.set(blog.author, 1)
+    }
+  }
+  //console.log(amounts)
+
+  const keyAndValue = [...amounts.entries()].reduce((prev, curr) => curr[1] > prev[1] ? curr : prev)
+  //console.log(keyAndValue)
+
+  const result = {
+    author: keyAndValue[0],
+    blogs: keyAndValue[1]
+  }
+
+  return result
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length == 0) {
+    return undefined
+  }
+
+  const amounts = new Map()
+
+  for (let i = 0; i < blogs.length; i++) {
+    const blog = blogs[i]
+
+    if (amounts.has(blog.author)) {
+      amounts.set(blog.author, amounts.get(blog.author) + blog.likes)
+    } else {
+      amounts.set(blog.author, blog.likes)
+    }
+  }
+  //console.log(amounts)
+
+  const keyAndValue = [...amounts.entries()].reduce((prev, curr) => curr[1] > prev[1] ? curr : prev)
+  //console.log(keyAndValue)
+
+  const result = {
+    author: keyAndValue[0],
+    likes: keyAndValue[1]
+  }
+
+  return result
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
