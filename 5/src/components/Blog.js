c759a7868dddeaae96008ba5c47d5eb1624a1e99
Julius Sakki <j.sakki@icloud.com>
2022-10-27 19:19:32 +0300

    5.7

--- 5/src/components/Blog.js
+++ 5/src/components/Blog.js
@@ -1,15 +1,34 @@
 import { useState } from 'react'
 
-const Blog = ({blog}) => (
-  //const [full, setFull] = useState(false)
+const Blog = ({ blog }) => {
+  const [full, setFull] = useState(false)
 
-  /*return (
-    
-  )*/
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
 
-  <div>
-      {blog.title} {blog.author}
-  </div>
-)
+  return (
+    <div style={blogStyle}>
+      {blog.title}
+      <button onClick={() => setFull(!full)}>
+        {full ? 'Hide' : 'View'}
+      </button>
+      {full &&
+        <div>
+          <p>
+            {blog.author}
+          </p>
+          <p>
+            {blog.url}
+          </p>
+        </div>
+      }
+    </div>
+  )
+}
 
 export default Blog
