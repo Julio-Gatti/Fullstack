862dfea36409dfc090494d9d6505f6974ef424ec
Julius Sakki <j.sakki@icloud.com>
2022-10-27 17:21:28 +0300

    5.4

--- 5/src/App.js
+++ 5/src/App.js
@@ -1,18 +1,27 @@
 import { useState, useEffect } from 'react'
+
 import loginService from './services/login'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('') 
+  const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setURL] = useState('')
+  const [notificationText, setNotificationText] = useState(null)
+  const [notificationColor, setNotificationColor] = useState('green')
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
+    blogService
+      .getAll()
+      .then(blogs =>
       setBlogs( blogs )
-    )  
+    )
   }, [])
 
   useEffect(() => {
@@ -45,10 +54,11 @@ const App = () => {
     } catch (exception) {
       console.log('Wrong credentials')
 
-      /*setErrorMessage('Wrong credentials')
+      setNotificationText('Wrong credentials')
+      setNotificationColor('red')
       setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)*/
+        setNotificationText(null)
+      }, 5000)
     }
   }
 
@@ -61,10 +71,49 @@ const App = () => {
     window.localStorage.clear()
   }
 
+  const handleNew = (event) => {
+    event.preventDefault()
+
+    console.log('Creating a blog')
+
+    const blog = {
+      title: title,
+      author: author,
+      url: url
+    }
+
+    try {
+      blogService
+        .create(blog)
+        .then(returnedBlog => {
+          setBlogs(blogs.concat(returnedBlog))
+          setTitle('')
+          setAuthor('')
+          setURL('')
+        })
+
+        setNotificationText(`Created blog ${blog.title}`)
+        setNotificationColor('green')
+        setTimeout(() => {
+          setNotificationText(null)
+        }, 5000)
+    } catch (exception) {
+      console.log('Failed to create blog')
+
+      setNotificationText('Failed to create blog')
+      setNotificationColor('red')
+      setTimeout(() => {
+        setNotificationText(null)
+      }, 5000)
+    }
+  }
+
   if (!user) {
     return (
       <div>
         <h2>Login</h2>
+
+        <Notification message={notificationText} color={notificationColor} />
 
         <form onSubmit={handleLogin}>
           <div>
@@ -95,10 +144,45 @@ const App = () => {
     <div>
       <h2>Blogs</h2>
 
+      <Notification message={notificationText} color={notificationColor} />
+
       <p>{user.name} logged in</p>
       <button onClick={() => handleLogOut()}>
         Log out
       </button>
+
+      <h2>Create new</h2>
+
+      <form onSubmit={handleNew}>
+          <div>
+            Title
+              <input
+              type="text"
+              value={title}
+              name="Username"
+              onChange={({ target }) => setTitle(target.value)}
+            />
+          </div>
+          <div>
+            Author
+              <input
+              type="text"
+              value={author}
+              name="Password"
+              onChange={({ target }) => setAuthor(target.value)}
+            />
+          </div>
+          <div>
+            URL
+              <input
+              type="text"
+              value={url}
+              name="Password"
+              onChange={({ target }) => setURL(target.value)}
+            />
+          </div>
+          <button type="submit">Create</button>
+        </form>
 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
